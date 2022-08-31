e5a11d1172bcc32d57073410e6d8cdaf7f5033a2
keeeeeey <sseioul@naver.com>
2022-08-31 16:16:43 +0900

    20220831 오후 수업 내용

--- django2/articles/views.py
+++ django2/articles/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        "articles" : articles
+    }
+    return render(request, "articles/index.html", context)
+
+def new(request):
+    return render(request, "articles/new.html")
+
+def create(request):
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    # 1
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
+
+    # 2
+    article = Article(title = title, content = content)
+    article.save()
+
+    # 3
+    # Article.objects.create(title = title, content = content)
+
+    return render(request, "articles/create.html")
