121877776c38731c48c4c31746ff68e57cf551d2
keeeeeey <sseioul@naver.com>
2022-09-01 17:33:52 +0900

    [20220901 4회차 과제]

--- day4/day4_problem1/articles/views.py
+++ day4/day4_problem1/articles/views.py
@@ -18,4 +18,30 @@ def create(request):
     article = Article(title=title, content=content)
     article.save()
     
+    return render(request, "articles/create.html")
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        "article" : article,
+    }
+    return render(request, "articles/detail.html", context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
     return redirect("articles:index")
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        "article" : article,
+    }
+    return render(request, "articles/edit.html", context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get("title")
+    article.content = request.POST.get("content")
+    article.save()
+    return redirect("articles:detail", article.pk)
