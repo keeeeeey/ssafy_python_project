0dfa67b353ca63e9ce513a2443ea8904221b7eb2
keeeeeey <sseioul@naver.com>
2022-09-10 11:57:16 +0900

    ws_6_3

--- ws6/project6-2/travels/views.py
+++ ws6/project6-2/travels/views.py
@@ -26,9 +26,32 @@ def create(request):
     }
     return render(request, "travels/create.html", context)
 
+@require_safe
 def detail(request, pk):
     travel = get_object_or_404(Travels, pk=pk)
     context = {
         "travel" : travel,
     }
     return render(request, "travels/detail.html", context)
+
+@require_POST
+def delete(request, pk):
+    travel = get_object_or_404(Travels, pk=pk)
+    travel.delete()
+    return redirect("travels:index")
+
+@require_http_methods(["GET", "POST"])
+def update(request, pk):
+    travel = get_object_or_404(Travels, pk=pk)
+    if request.method == "POST":
+        form = TravelForm(request.POST, instance=travel)
+        if form.is_valid():
+            form.save()
+            return redirect("travels:detail", travel.pk)
+    else:
+        form = TravelForm(instance=travel)
+    context = {
+        "form" : form,
+        "travel" : travel,
+    }
+    return render(request, "travels/update.html", context)
