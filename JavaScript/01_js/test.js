2d08cd484b5086d755a6ed49fc586763cf76a233
keeeeeey <sseioul@naver.com>
2022-10-19 20:08:48 +0900

    20221019 자바스크립트 과제

---
+++ JavaScript/01_js/test.js
@@ -0,0 +1,13 @@
+// for (let i = 0; i < 5; i++) {
+//     console.log(i)
+// }
+// const nums = [1, 2, 3, 4, 5]
+
+// for (num of nums) {
+//   console.log(num, typeof num)
+// }
+
+const nums = [1, 2, 3, 4]
+
+const new_nums = nums.map(num => num ** 3)
+console.log(new_nums)
--- django/firstpjt/firstpjt/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include # include 가 url 나누기의 핵심 기능
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # 화면 추가 path("경로", 해당 경로)
-    path('articles/', include('articles.urls')),
-    path('pages/', include('pages.urls')),
-]
