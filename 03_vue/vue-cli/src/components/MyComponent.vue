e034d139f436e2c7efe8e1bc992a0873135cb3f5
keeeeeey <sseioul@naver.com>
2022-11-02 11:51:32 +0900

    20221102 오전 수업

---
+++ 03_vue/vue-cli/src/components/MyComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="border">
+        <h1>싸피 이거는 내가 만든 새로운 컴포넌트다!</h1>
+        <MyComponentItem/>
+    </div>
+</template>
+
+<script>
+import MyComponentItem from '@/components/MyComponentItem';
+
+export default {
+    name: "MyComponent",
+    components: {
+        MyComponentItem,
+    }
+}
+</script>
+
+<style>
+    .border {
+        border: solid 1px black;
+    }
+</style>
---
+++ 03_vue/vue-cli/src/components/MyComponentItem.vue
@@ -0,0 +1,18 @@
+<!-- 1. 파일 생성 -->
+<template>
+    <!-- 3. 최상위 태그 추가 -->
+    <div>
+        <h3>나는 MyComponent의 하위 컴포넌트</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    // 2. 이름 설정
+    name: "MyComponentItem",
+}
+</script>
+
+<style>
+    
+</style>
